ec5fa8954400a4abe9831937d33ff816a0aa4e68
Irman Maulana <47386651+zicron69@users.noreply.github.com>
2021-09-20 05:48:50 +0700

    Initial commit

---
+++ app/src/main/java/com/real/dbci_apk/dashboard.java
@@ -0,0 +1,13 @@
+package com.real.dbci_apk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashbard_view);
+    }
+}
